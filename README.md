57fc55df3c011cca493dfb55b6609658d442ea59
cpuCode <923992029@qq.com>
2021-11-22 09:39:48 +0800

    数据类型

--- README.md
+++ README.md
@@ -23,6 +23,7 @@ Scala语言
 - [x] [字符串输出](data_type/src/main/scala/com/cpucode/string/StringTest.scala)
 - [x] [键盘输入](data_type/src/main/scala/com/cpucode/stdin/StdInTest.scala)
 - [x] [文件读取](data_type/src/main/scala/com/cpucode/file/io/FileIO.scala)
+- [x] [数据类型](data_type/src/main/scala/com/cpucode/dataType/DataType.scala)
 
 ----------------------
 
---
+++ data_type/src/main/scala/com/cpucode/dataType/DataType.scala
@@ -0,0 +1,43 @@
+package com.cpucode.dataType
+
+/**
+ * @author : cpucode
+ * @date : 2021/11/22 9:32
+ * @github : https://github.com/CPU-Code
+ * @csdn : https://blog.csdn.net/qq_44226094
+ */
+object DataType {
+  def main(args: Array[String]): Unit = {
+    // 1. 整数类型
+    val a1: Byte = 127
+    val a2: Byte = -128
+
+//    val a3: Byte = 128 // error
+//    val a33: Byte = -129
+
+    println(a1)
+    println(a2)
+
+
+    // 整数默认类型为Int
+    val a4 = 10
+    // 长整型数值定义
+    val a5: Long = 1234567891023L
+
+    println(a4)
+    println(a5)
+
+
+    val b1: Byte = 20
+    val b2: Byte = 10 + 20
+    println(b1)
+    println(b2)
+
+//    val b3: Byte = b1 + 10
+    val b4: Byte = (b1 + 20).toByte
+
+    println(b4)
+
+  }
+
+}
